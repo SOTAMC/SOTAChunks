0eda0bfd304bc011282fd0719589faebd16156ea
BuildTools <unconfigured@null.spigotmc.org>
2022-03-27 14:02:04 -0400

    Initial Commit T2

--- src/main/java/git/sotamc/sotalighting/SOTALighting.java
+++ src/main/java/git/sotamc/sotalighting/SOTALighting.java
@@ -2,6 +2,6 @@ package git.sotamc.sotalighting;
 
 public class SOTALighting {
     public static void main(String[] args) {
-        System.out.println("Test main");
+        System.out.println("Main Main Class");
     }
 }
--- src/test/java/git/sotamc/sotalighting/SOTALightingTest.java
+++ src/test/java/git/sotamc/sotalighting/SOTALightingTest.java
@@ -1,4 +1,7 @@
 package git.sotamc.sotalighting;
 
 public class SOTALightingTest {
+    public static void main(String[] args) {
+        System.out.println("Main Test Class");
+    }
 }
